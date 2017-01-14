e10786626ca9a176353ca213b8a085349423a2d3
TimCoding <timbvho@gmail.com>
2017-01-14 04:33:47 -0600

    integrated model

--- server.go
+++ server.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	//third party packages
+	"github.com/TimCoding/rest-go-api-tutorial/models"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -12,8 +14,19 @@ func main() {
 	//instantiate a new router
 	r := httprouter.New()
 
-	r.GET("/test", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-		fmt.Fprint(w, "Welcome\n")
+	r.GET("/user/:id", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		u := models.User{
+			Name:   "Bob Smith",
+			Gender: "male",
+			Age:    50,
+			Id:     p.ByName("id"),
+		}
+
+		uj, _ := json.Marshal(u)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		fmt.Fprintf(w, "%s\n", uj)
+
 	})
 
 	http.ListenAndServe("localhost:3000", r)
